94a4e812ff27b906b84badc6d1bf67a918da2790
Ali Ensar Can <aliensarcan0@gmail.com>
2024-07-17 14:30:12 +0300

    YorumEklemeFormu

--- obj/Debug/net8.0/BogApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0698d4f0eb3ddf711be8f79d2e752fe094d8e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34138639f9940dce18b2c429dd1445d0caa996cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
